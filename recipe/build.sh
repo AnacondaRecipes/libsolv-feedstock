a4e4a48b7ae0f982c8b9f7087d8dd381608008d5
Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
2023-09-07 18:10:06 +0300

    [PKG-2779] libsolv 0.7.24 (#3)
    * libsolv 0.7.24
    
    * Add a comment about pkg-config

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,15 +9,15 @@ cd build || exit 1
 # Generate the build files.
 echo "Generating the build files..."
 cmake .. ${CMAKE_ARGS} \
-      -GNinja \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DENABLE_CONDA=ON \
-      -DMULTI_SEMANTICS=ON \
-      -DENABLE_PCRE2=ON \
-      -DENABLE_STATIC=ON
+-GNinja \
+-DCMAKE_PREFIX_PATH=$PREFIX \
+-DCMAKE_INSTALL_PREFIX=$PREFIX \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_LIBDIR=lib \
+-DENABLE_CONDA=ON \
+-DMULTI_SEMANTICS=ON \
+-DENABLE_PCRE2=ON \
+-DENABLE_STATIC=ON
 
 
 # Build.
